6cee093555aa6f217a6555fdf4a9e2e76c26c955
Dragoș-Andrei Dumitraș <59060738+Dragos-Andrei-75@users.noreply.github.com>
2025-02-14 12:03:45 +0200

    Imported Spring Initializr Package
    Imported Spring Initializr Package.
    
    Spring Initializr Package include the following dependencies:
    - Spring Web (WEB);
    - Spring Data JPA (WEB);
    - PostgreSQL Driver (SQL);

---
+++ src/main/java/com/example/Spring_Initializr_Bookstore/SpringInitializrBookstoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Initializr_Bookstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInitializrBookstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInitializrBookstoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring_Initializr_Bookstore/SpringInitializrBookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring_Initializr_Bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInitializrBookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
