4f2824a20ed9e31da84e5e7cbbfd3b61f0ec4b5e
Dragoș-Andrei Dumitraș <59060738+Dragos-Andrei-75@users.noreply.github.com>
2025-02-16 01:30:43 +0200

    Modified User Classes
    Modified classes:
    - UserService
        - defined update and delete
          methods;
        - modified the create method in
          order hash the user's password
          using the md5DigestAsHex
          method;
    - UserController
        - defined update and delete
          methods;

--- src/main/java/com/example/Spring_Initializr_Bookstore/controller/UserController.java
+++ src/main/java/com/example/Spring_Initializr_Bookstore/controller/UserController.java
@@ -3,7 +3,9 @@ package com.example.Spring_Initializr_Bookstore.controller;
 import com.example.Spring_Initializr_Bookstore.entities.User;
 import com.example.Spring_Initializr_Bookstore.entitiesDTO.UserDTO;
 import com.example.Spring_Initializr_Bookstore.mapper.UserMapper;
+import com.example.Spring_Initializr_Bookstore.repositories.UserRepository;
 import com.example.Spring_Initializr_Bookstore.service.UserService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,8 @@ import java.util.List;
 @RestController()
 @RequestMapping(path = "/users")
 public class UserController {
+    @Autowired()
+    private UserRepository userRepository;
     @Autowired()
     private UserService userService;
 
@@ -33,9 +37,25 @@ public class UserController {
 
     @GetMapping(path = "/find-all-users")
     public ResponseEntity<?> findAll() {
-        List<User> users = userService.findAll();
-        List<UserDTO> userDTOS = users.stream().map(UserMapper::user2UserDTO).toList();
+        List<User> foundUsers = userService.findAll();
+        List<UserDTO> userDTOS = foundUsers.stream().map(UserMapper::user2UserDTO).toList();
 
         return ResponseEntity.ok(userDTOS);
+    }
+
+    @PutMapping(path = "/update-user/{userID}")
+    public ResponseEntity<?> update(@PathVariable(name = "userID") Long userID, @RequestBody UserDTO userDTO) {
+        User userToUpdate = userRepository.findById(userID).orElseThrow(() -> new EntityNotFoundException("User with ID " + userID + " not found."));
+        User updatedUser = userService.update(userToUpdate, userDTO);
+
+        return ResponseEntity.ok(UserMapper.user2UserDTO(updatedUser));
+    }
+
+    @DeleteMapping(path = "/delete-user/{userID}")
+    public ResponseEntity<?> delete(@PathVariable(name = "userID") Long userID) {
+        User user = userRepository.findById(userID).orElseThrow(() -> new EntityNotFoundException("User with ID " + userID + " not found."));
+        userService.delete(user);
+
+        return ResponseEntity.ok().body("Deleted user with ID " + userID + ".");
     }
 }
--- src/main/java/com/example/Spring_Initializr_Bookstore/service/UserService.java
+++ src/main/java/com/example/Spring_Initializr_Bookstore/service/UserService.java
@@ -1,11 +1,14 @@
 package com.example.Spring_Initializr_Bookstore.service;
 
 import com.example.Spring_Initializr_Bookstore.entities.User;
+import com.example.Spring_Initializr_Bookstore.entitiesDTO.UserDTO;
 import com.example.Spring_Initializr_Bookstore.repositories.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Service()
@@ -15,6 +18,7 @@ public class UserService {
 
     public User create(User user) {
         if (user.getId() != null) throw new RuntimeException("Cannot provide an ID when creating a new user.");
+        user.setPassword(DigestUtils.md5DigestAsHex(user.getPassword().getBytes(StandardCharsets.UTF_8)));
         return userRepository.save(user);
     }
 
@@ -24,5 +28,21 @@ public class UserService {
 
     public List<User> findAll() {
         return userRepository.findAll();
+    }
+
+    public User update(User userToUpdate, UserDTO userUpdate) {
+        userToUpdate.setFirstName(userUpdate.getFirstName());
+        userToUpdate.setLastName(userUpdate.getLastName());
+        userToUpdate.setGender(userUpdate.getGender());
+        userToUpdate.setCountry(userUpdate.getCountry());
+        userToUpdate.setYearOfBirth(userUpdate.getYearOfBirth());
+        userToUpdate.setEmail(userUpdate.getEmail());
+        userToUpdate.setPhoneNumber(userUpdate.getPhoneNumber());
+
+        return userRepository.save(userToUpdate);
+    }
+
+    public void delete(User user) {
+        userRepository.delete(user);
     }
 }
